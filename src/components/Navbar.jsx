acc51abc2a1607dd49e2d68bba12e4025fee1dcc
LindaEng <90lineng@gmail.com>
2023-10-05 21:02:46 -0400

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  const [active, setActive] = useState('')
+      return (
+        <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+          <div className="flex justify-between items-center max-w-7xl mx-auto w-full">
+            <Link
+              to="/"
+              className="flex items-center gap-2"
+              onClick={() => {
+                setActive("");
+                window.scrollTo(0,0);
+              }}
+            >
+              <img src={logo} alt="logo" className="w-4 h-4 object-contain"/>
+            </Link>
+          </div>
+        </nav>
+      )
 }
 
 export default Navbar
