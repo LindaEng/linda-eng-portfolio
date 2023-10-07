7bd4eda64417bb38293e08776a400929e2a362d5
LindaEng <90lineng@gmail.com>
2023-10-07 13:32:14 -0400

    nav complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,9 +21,9 @@ const Navbar = () => {
               }}
             >
               <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-              <p className="text-white text-[18px] font-bold cursor-pointer">Linda <span className="sm:block hidden"> | Software Engineer</span></p>
+              <p className="text-white text-[18px] font-bold cursor-pointer flex">Linda &nbsp; <span className="sm:block hidden"> |  Software Engineer</span></p>
             </Link>
-            <ul className="list-non hidden sm:flex flex-row gap-10">
+            <ul className="list-none hidden sm:flex flex-row gap-10">
               {navLinks.map((link) => (
                 <li
                   key={link.id}
@@ -48,7 +48,26 @@ const Navbar = () => {
                 onClick={() => setToggle(!toggle)}
               />
               <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-
+                <ul className="list-none flex justify-end items-start flex-col gap-4">
+                  {navLinks.map((link) => (
+                    <li
+                      key={link.id}
+                      className={`${
+                        active === link.title
+                        ? "text-white"
+                        : "text-secondary"
+                      } font-poppins font-medium
+                      cursor-pointer text-[16px]
+                      `}
+                      onClick={() => {
+                        setToggle(!toggle)
+                        setActive(link.title)
+                      }}
+                    >
+                      <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
