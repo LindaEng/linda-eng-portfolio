c8b470d492cdf870efad51c118cf4d9054e2c5c1
LindaEng <90lineng@gmail.com>
2024-01-16 19:09:01 -0500

    front end components added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Linda Eng</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a FullStack developer.<br className="sm:block hidden" /> Check out my portfolio below!
+            I'm a Software Engineer.<br className="sm:block hidden" /> Check out my portfolio below!
           </p>
         </div>
       </div>
